31421d4c1b326fffaf4be3e07bfb8c2fce30735d
Nicolai8 <bolotin.nicolai@gmail.com>
2016-01-14 17:06:14 +0300

    angular2 replace HTML5 routing with Hast routing

--- public/ts/app/boot.ts
+++ public/ts/app/boot.ts
@@ -2,8 +2,12 @@ import {bootstrap}    from "angular2/platform/browser";
 import {AppComponent} from "./app.component";
 import {ROUTER_PROVIDERS} from "angular2/router";
 import {HeroService} from "./hero.service";
+import {LocationStrategy,
+    HashLocationStrategy} from "angular2/router";
+import {provide} from "angular2/core";
 
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
-    HeroService
+    HeroService,
+    provide(LocationStrategy, {useClass: HashLocationStrategy})
 ]);
--- routes/profile.js
+++ routes/profile.js
@@ -1,14 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
-//get my profile
-router.get("/", function (req, res, next) {
-	res.json("my profile");
-});
-
 //get profile by id
 router.get("/:id", function (req, res, next) {
 	res.json("profile " + req.params.id);
+});
+
+//get my profile
+router.get("/", function (req, res, next) {
+	res.json("my profile");
 });
 
 //save profile
