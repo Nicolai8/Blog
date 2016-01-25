78fd47026f52c9462930f529ace565c445d9b06c
Nicolai8 <bolotin.nicolai@gmail.com>
2016-01-25 12:43:07 +0300

    add search by article name/username

--- models/article.js
+++ models/article.js
@@ -1,4 +1,5 @@
 var mongoose = require("../lib/mongoose");
+var async = require("async");
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
@@ -12,16 +13,32 @@ var schema = new Schema({
 
 schema.statics.getPage = function (searchQuery, pageIndex, pageSize, callback) {
 	var skip = (pageIndex - 1 ) * pageSize;
+	var Article = this;
 
-	this.find({"title": new RegExp(searchQuery, "i")})
-		.populate({
-			path: "_owner",
-			select: "username",
-			match: {"username": new RegExp(searchQuery, "i")}
-		})
-		.sort("-created")
-		.skip(skip).limit(pageSize)
-		.exec(callback);
+	async.waterfall([
+		function (cb) {
+			mongoose.models.User.find({"username": new RegExp(searchQuery, "i")}).select("_id").exec(cb);
+		},
+		function (users, cb) {
+			Article.find({
+					$or: [
+						{"title": new RegExp(searchQuery, "i")},
+						{
+							"_owner": {
+								$in: users
+							}
+						}
+					]
+				})
+				.populate({
+					path: "_owner",
+					select: "username"
+				})
+				.sort("-created")
+				.skip(skip).limit(pageSize)
+				.exec(cb);
+		}
+	], callback);
 };
 
 schema.statics.getPageByUserId = function (userId, pageIndex, pageSize, callback) {
--- public/ts/app/components/app.component.ts
+++ public/ts/app/components/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from "angular2/core";
-import {RouteConfig, ROUTER_DIRECTIVES} from "angular2/router";
+import {Router, RouteConfig, ROUTER_DIRECTIVES} from "angular2/router";
 import {HomeComponent} from "./home.component";
 import {ArticleDetailComponent} from "./article-detail.component";
 import {LoginComponent} from "./login.component";
@@ -8,6 +8,7 @@ import {ProfileComponent} from "./profile.component";
 import {UserArticlesComponent} from "./user-articles.component";
 import {CreateArticleComponent} from "./create-article.component";
 import {cleanBlog} from "../../plugins/clean-blog";
+import {SearchComponent} from "./search.component";
 
 @Component({
     selector: "my-app",
@@ -21,17 +22,23 @@ import {cleanBlog} from "../../plugins/clean-blog";
     {path: "/article/:id", name: "ArticleDetail", component: ArticleDetailComponent},
     {path: "/profile/:id/articles", name: "UserArticles", component: UserArticlesComponent},
     {path: "/profile/:id", name: "Profile", component: ProfileComponent},
-    {path: "/create-article", name: "CreateArticle", component: CreateArticleComponent}
+    {path: "/create-article", name: "CreateArticle", component: CreateArticleComponent},
+    {path: "/search/:searchString", name: "Search", component: SearchComponent}
 ])
 
 export class AppComponent implements OnInit {
     public isAuthorized:boolean = false;
 
-    constructor(private _authService:AuthService) {
+    constructor(private _authService:AuthService,
+                private _router:Router) {
     }
 
     ngOnInit() {
         this._authService.isAuthorized.subscribe(isAuthorized=>this.isAuthorized = isAuthorized);
         cleanBlog();
+    }
+
+    search(searchString:string) {
+        this._router.navigate(["Search", {searchString: searchString}]);
     }
 }
