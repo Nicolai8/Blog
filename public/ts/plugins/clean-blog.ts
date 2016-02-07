a0b2688e3262d5e49b763f9dce72f35eea5fb0a9
Nicolai8 <bolotin.nicolai@gmail.com>
2016-02-07 16:11:43 +0300

    style forms

--- public/ts/plugins/clean-blog.ts
+++ public/ts/plugins/clean-blog.ts
@@ -33,4 +33,12 @@ export function cleanBlog() {
                 this.previousTop = currentTop;
             });
     }
+
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
 }
